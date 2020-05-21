a4e312d9886ad092b9650b6cd0a2c90cfebd99b3
lixiaoxue1 <lixiaoxue1@360.cn>
2020-05-21 16:40:01 +0800

    add build

---
+++ build/precache-manifest.538dad84fa3cc1c7e7c9484ba1c408f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaa0897c616c53c0ab2a904a30324de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fc6278da9cdc2a80f5f",
+    "url": "./static/css/main.fc970746.chunk.css"
+  },
+  {
+    "revision": "a08153ce7b9402fc74bc",
+    "url": "./static/js/2.40931a3c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.40931a3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc6278da9cdc2a80f5f",
+    "url": "./static/js/main.afc77b4f.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  }
+]);
